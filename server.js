03417bc5df27e50d10f786efb82b8c73289485a9
Todd McKenzie <toddmckenzie@hotmail.com>
2020-04-19 16:45:46 -0500

    added replies route

--- server.js
+++ server.js
@@ -11,10 +11,11 @@ server.use(cors());
 const login = require('./routes/login');
 const channel = require('./routes/channels');
 const messages = require('./routes/messages');
-
+const replies = require('./routes/replies')
 
 server.use('/login', login);
 server.use('/channels', channel)
 server.use('/messages', messages)
+server.use('/replies', replies)
 
 module.exports = server;
