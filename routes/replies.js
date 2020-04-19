27a30b4af8ae70df0de0bf4fcdf9ab956665dc06
Todd McKenzie <toddmckenzie@hotmail.com>
2020-04-19 16:50:26 -0500

    added ability to delte in replies models

--- routes/replies.js
+++ routes/replies.js
@@ -20,4 +20,17 @@ router.post('/', (req, res) => {
 })
 
 
+router.delete('/:id', (req, res) => {
+    const id = req.params.id;
+
+    db
+    .remove(id)
+    .then(result => {
+        res.status(200).json(result)
+    })
+    .catch(err => {
+        res.status(500).json({ message: "Internal Server Error"})
+    })
+})
+
 module.exports = router;
