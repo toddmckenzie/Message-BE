d0529d88daadc665b018ba2446ce11c146ef98fa
Todd McKenzie <toddmckenzie@hotmail.com>
2020-04-20 17:22:37 -0500

    fixed in channels models

--- models/channels.js
+++ models/channels.js
@@ -16,5 +16,5 @@ function remove(id){
 
 //need to join with channels tables.....
 function getUserChannels(id){
-    return db('user_channels').where('user_id', id).join('channels', 'user_channels.channel_id').select('*');
+    return db('user_channels').where({ user_id: id }).join('channels', 'user_channels.channel_id').select('*');
 }
