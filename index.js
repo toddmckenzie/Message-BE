a4f8c4e539ff8a685c9fb8bf54729145695f66e0
Todd McKenzie <toddmckenzie@hotmail.com>
2020-04-26 14:53:37 -0500

    testing db

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const server = require('./server.js');
 
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 4003;
 
 server.listen(port, () => {
     console.log(`Server is listening on port ${port}`)
--- models/dms.js
+++ models/dms.js
@@ -6,16 +6,14 @@ module.exports = {
     find
 }
 
-//need to test //
 function add(message){
     return db('dms').insert(message);
 }
 
-//need to test //
 function find(id){
     return db('dm_user').where({ user_id: id }).join('dms', 'user_id', '=', id)
 }
-//need to test also // 
+
 function remove(id){
     return db('dms').where({ id }).del();
 }
